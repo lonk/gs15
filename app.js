405a8adc7b12a6d631df9cf83601125e0f8d3320
Lonk <rinke2005@gmail.com>
2016-10-28 19:51:25 +0200

    Ask user for wanted algo

--- app.js
+++ app.js
@@ -1,1 +1,49 @@
 import BigNumber from 'bignumber.js';
+import inquirer  from 'inquirer';
+
+inquirer.prompt([
+    {
+        type   : 'list',
+        name   : 'algo',
+        message: 'Selectionnez votre fonction de chiffrement',
+        choices: [
+            {
+                key  : 1,
+                name : 'Chiffrement symétrique VCES',
+                value: 1
+            },
+            {
+                key     : 2,
+                name    : 'Chiffrement RSA avec module multiple',
+                disabled: 'Indisponible pour le moment',
+                value   : 2
+            },
+            {
+                key     : 3,
+                name    : 'Chiffrement RSA avec module multiple',
+                disabled: 'Indisponible pour le moment',
+                value   : 3
+            },
+            {
+                key     : 4,
+                name    : 'Déchiffrement RSA',
+                disabled: 'Indisponible pour le moment',
+                value   : 4
+            },
+            {
+                key     : 5,
+                name    : 'Vérifier une signature RSA',
+                disabled: 'Indisponible pour le moment',
+                value   : 5
+            }
+        ]
+    }
+]).then(answers => {
+    switch(answers.algo) {
+        case 1:
+            console.log('VCES');
+            break;
+        default:
+            console.log('Algorithme indisponible')
+    }
+});
