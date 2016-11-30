12b0d7c5fb5a69e1f7f6bc5e2f13afa71dd0717b
Lonk <rinke2005@gmail.com>
2016-11-30 11:27:39 +0100

    Generate Galois vectors for AES

---
+++ src/aes.js
@@ -0,0 +1,75 @@
+// Retourne le degré d'un polynome
+function degree(poly) {
+    let degree = -1;
+    let i      = 0;
+
+    while (degree == -1 && i < poly.length) {
+        if (poly[i] > 0) {
+            degree = poly.length - i - 1;
+        }
+        i++;
+    }
+
+    return degree;
+}
+
+// Retourne le produit de deux polynomes dans Rjindael
+function multiplyRjindael(polyA, polyB) {
+    const degreeA = degree(polyA);
+    const degreeB = degree(polyB);
+
+    const revertedA = JSON.parse(JSON.stringify(polyA)).reverse();
+    const revertedB = JSON.parse(JSON.stringify(polyB)).reverse();
+
+    let revertedR = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+    for (let iA = 0; iA <= degreeA; iA++) {
+        if (revertedA[iA] > 0) {
+            for (let iB = 0; iB <= degreeB; iB++) {
+                if (revertedB[iB] > 0) {
+                    revertedR[iA + iB] += 1;
+                }
+            }
+        }
+    }
+
+    if (revertedR[8] == 1) { // L'opération de modulo est équivalente à remplacer X^8 par X^4 + X^3 + X + 1
+        revertedR[4] += 1;
+        revertedR[3] += 1;
+        revertedR[1] += 1;
+        revertedR[0] += 1;
+    }
+
+    revertedR = revertedR.slice(0, 8);
+
+    revertedR = revertedR.map(x => {
+        if (x % 2 == 0) {
+            return 0;
+        }
+        return 1;
+    });
+
+    return revertedR.reverse();
+}
+
+// Génère la table de Rjindael
+function generationRjindael() {
+    let table = [
+        [0, 0, 0, 0, 0, 0, 0, 0], // élément nul
+        [0, 0, 0, 0, 0, 0, 0, 1], // polynome générateur à la puissance 0
+        [0, 0, 0, 0, 0, 0, 1, 1]  // polynome générateur
+    ];
+
+    const generator = table[2];
+
+    // Il y a 255 éléments non nuls dans la table. On en connaît déjà 2, il faut trouver les 253 autres
+    for (let i = 2; i < 255; i++) {
+        table.push(multiplyRjindael(table[i], generator));
+    }
+
+    return table;
+}
+
+const gf256 = generationRjindael();
+
+console.log(gf256);
