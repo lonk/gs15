03b720951fe917b1d44482b676e80fafe81d519e
Lonk <rinke2005@gmail.com>
2016-12-28 18:21:39 +0100

    Generate RSA/PKCS#1 public and private keys

--- src/rsa.js
+++ src/rsa.js
@@ -14,3 +14,46 @@ function largePrimeGenerator() {
     return largePrimeGenerator();
 }
 
+function eGenerator(p, q, phin) {
+    let e = null;
+    let i = p.plus(1);
+    if (q.gt(p)) {
+        i = q.plus(1);
+    }
+
+    while (!e && i.lt(phin)) {
+        if (bigInt.gcd(i, phin).eq(1)) {
+            e = i;
+        }
+
+        i = i.plus(1);
+    }
+
+    return e;
+}
+
+function keysGenerator() {
+    const p    = largePrimeGenerator();
+    const q    = largePrimeGenerator();
+    const n    = p.multiply(q);
+    const phin = p.minus(1).multiply(q.minus(1));
+    const e    = eGenerator(p, q, phin);
+    const d    = e.modInv(phin);
+    const dp   = d.mod(p.minus(1));
+    const dq   = d.mod(q.minus(1));
+    const qinv = q.modInv(p);
+
+    return {
+        public: {
+            e: e.toString(),
+            n: n.toString()
+        },
+        private: {
+            p   : p.toString(),
+            q   : q.toString(),
+            dp  : dp.toString(),
+            dq  : dq.toString(),
+            qinv: qinv.toString()
+        }
+    };
+}
