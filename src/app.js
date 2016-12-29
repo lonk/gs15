b6cfce0c06976a466e57bbd7833582f3fab90649
Lonk <rinke2005@gmail.com>
2016-12-29 16:01:40 +0100

    Allow user to crypt and uncrypt using RSA

--- src/app.js
+++ src/app.js
@@ -37,16 +37,14 @@ function ask() {
                     value: 3
                 },
                 {
-                    key     : 4,
-                    name    : 'Chiffrement RSA avec module multiple',
-                    disabled: 'Indisponible pour le moment',
-                    value   : 4
+                    key  : 4,
+                    name : 'Chiffrement RSA avec module multiple',
+                    value: 4
                 },
                 {
-                    key     : 5,
-                    name    : 'Déchiffrement RSA',
-                    disabled: 'Indisponible pour le moment',
-                    value   : 5
+                    key  : 5,
+                    name : 'Déchiffrement RSA',
+                    value: 5
                 },
                 {
                     key     : 6,
@@ -95,6 +93,14 @@ function ask() {
                 algo = vces;
                 type = 'uncrypt';
                 break;
+            case 4:
+                algo = rsa;
+                type = 'crypt';
+                break;
+            case 5:
+                algo = rsa;
+                type = 'uncrypt';
+                break;
             case 8:
                 algo = des;
                 type = 'crypt';
@@ -132,17 +138,55 @@ function ask() {
             });
         }
 
+        if ([4, 5].indexOf(answers.algo) !== -1) {
+            inquirer.prompt([
+                {
+                    type   : 'input',
+                    name   : 'text',
+                    message: `Entrez le texte à ${(type == 'uncrypt' ? 'dé' : '')}chiffrer (défaut: ${lastResult})`
+                },
+                {
+                    type   : 'input',
+                    name   : 'alias',
+                    message: 'Entrez l\'alias à utiliser pour lire les clés'
+                }
+            ]).then(data => {
+                const file = `${data.alias}.${(type == 'uncrypt' ? 'private' : 'public')}`;
+
+                fs.readFile(file, (err, keyFile) => {
+                    if (err) throw err;
+
+                    if (!data.text) {
+                        data.text = lastResult;
+                    }
+
+                    const key = JSON.parse(keyFile);
+
+                    const result = algo(data.text, key, type);
+                    if (result.type == 'error') {
+                        console.log(`Erreur: ${result.data}`);
+                        return ask();
+                    }
+
+                    lastResult = result.data;
+                    console.log(`Le résultat ${(type == 'uncrypt' ? 'dé' : '')}chiffré est: ${JSON.stringify(result.data)}`);
+
+                    setTimeout(ask, 1000);
+                });
+            });
+        }
+
         if ([1, 2, 8, 9, 10, 11].indexOf(answers.algo) !== -1) {
             inquirer.prompt([
                 {
                     type   : 'input',
                     name   : 'text',
-                    message: `Entrez le texte à ${(type == 'uncrypt' ? 'dé' : '')}chiffrer (ne rien entrer si vous souhaitez copier le dernier résultat)`
+                    message: `Entrez le texte à ${(type == 'uncrypt' ? 'dé' : '')}chiffrer (défaut: ${lastResult})`
                 },
                 {
                     type   : 'input',
                     name   : 'key',
-                    message: `Entrez la clé de ${(type == 'uncrypt' ? 'dé' : '')}chiffrement (ne rien entrer si vous souhaitez garder la même clé)`
+                    message: `Entrez la clé de ${(type == 'uncrypt' ? 'dé' : '')}chiffrement (défaut: ${lastKey})`
                 }
             ]).then(data => {
                 if (!data.text) {
